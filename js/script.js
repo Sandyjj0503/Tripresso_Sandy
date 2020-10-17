72db3f00d602b7b039ef3bf491993d12f37ae263
Sandyjj0503 <s168923@gmail.com>
2020-10-18 01:41:30 +0800

    修正排序功能已及頁碼

--- js/script.js
+++ js/script.js
@@ -1,20 +1,121 @@
 var vm = new Vue({
     el: "#main",
     data: {
-        totalRecord: "100",
-        totalPage: "10",
+        totalRecord: "30",
+        totalPage: "3",
         dataList: [],
     },
     created: function () {
         this.getList();
     },
     methods: {
+        rating_asc: function () {
+            $(".searchParams").attr("value","rating_asc");
+            var searchParams = $(".searchParams").val();
+            var searchParam = "?sort=rating_asc";
+            var sort = vm.getSearchParams("sort") != undefined ? vm.getSearchParams("sort") : "";
+
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            var ratingParam = url.searchParams.get('sort');
+
+           
+            if (sort.length == 0 ) {
+                window.location.href =location.href + searchParam;
+            } else if (sort.length > 0 && ratingParam == "rating_desc"){
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString + "?sort=rating_asc";
+            } else {
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString +"?sort="+ searchParams;
+            }
+        },
+        rating_desc: function () {
+            $(".searchParams").attr("value","rating_desc");
+            var searchParams = $(".searchParams").val();
+            var searchParam = "?sort=rating_desc";
+            var sort = vm.getSearchParams("sort") != undefined ? vm.getSearchParams("sort") : "";
+           
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            var ratingParam = url.searchParams.get('sort');
+            
+            if (sort.length == 0 ) {
+                window.location.href =location.href + searchParam;
+            } else if (sort.length > 0 && ratingParam == "rating_asc"){
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString + "?sort=rating_desc";
+            } else {
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString +"?sort="+ searchParams;
+            }
+        },
+        price_asc: function () {
+            $(".searchParams").attr("value","price_asc");
+            var searchParams = $(".searchParams").val();
+            var searchParam = "?sort=price_asc";
+            var sort = vm.getSearchParams("sort") != undefined ? vm.getSearchParams("sort") : "";
+
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            var priceParam = url.searchParams.get('sort');
+
+           
+            if (sort.length == 0 ) {
+                window.location.href =location.href + searchParam;
+            } else if (sort.length > 0 && priceParam == "price_desc"){
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString + "?sort=price_asc";
+            } else {
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString +"?sort="+ searchParams;
+            }
+        },
+        price_desc: function () {
+            $(".searchParams").attr("value","price_desc");
+            var searchParams = $(".searchParams").val();
+            var searchParam = "?sort=price_desc";
+            var sort = vm.getSearchParams("sort") != undefined ? vm.getSearchParams("sort") : "";
+           
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            var priceParam = url.searchParams.get('sort');
+            
+            if (sort.length == 0 ) {
+                window.location.href =location.href + searchParam;
+            } else if (sort.length > 0 && priceParam == "price_asc"){
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString + "?sort=price_desc";
+            } else {
+                var arr = getUrlString.split("?"); 
+                getUrlString = arr[0];
+                window.location.href =getUrlString +"?sort="+ searchParams;
+            }
+        },
         getList: function () {
             var vm = this;
             var page = vm.getSearchParams("page") != undefined ? vm.getSearchParams("page") : "1";
             if (!$.isNumeric(page)) { page = "1"; }
+
+            var getUrlString = location.href;
+            var url = new URL(getUrlString);
+            var searchParams = url.searchParams.get('sort');
+            $(".searchParams").attr("value",searchParams);
+            console.log(searchParams)
+
             // //組合param
             var searchParam = "";
+
+            var sort = vm.getSearchParams("sort") != undefined ? vm.getSearchParams("sort") : "";
+            if (sort.length > 0 && searchParams != "") { searchParam += "&sort=" + searchParams; }
+            
 
             var url = "https://cors-anywhere.herokuapp.com/https://interview.tripresso.com/tour/search?row_per_page=10&page="+ page + searchParam;
             $.ajax({
